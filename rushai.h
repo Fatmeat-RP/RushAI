9753fe1cbc51e53d11e6651498b591bb67568f69
Nicolas Jaros <njaros@z4r9p5.42lyon.fr>
2021-11-06 15:02:11 +0100

    modif .h

--- rushai.h
+++ rushai.h
@@ -6,12 +6,15 @@
 /*   By: njaros <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 13:01:26 by acarle-m          #+#    #+#             */
-/*   Updated: 2021/11/06 14:04:40 by njaros           ###   ########lyon.fr   */
+/*   Updated: 2021/11/06 14:47:13 by njaros           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RUSHAI_H
 # define RUSHAI_H
+
+# include <unistd.h>
+# include <stdlib.h>
 
 /*			struct			*/
 
@@ -27,20 +30,26 @@ typedef struct	s_parms
 
 typedef struct	s_amove
 {
-	int x;
-	int y;
-	int	etats;
-	s_amove	*next;
-	s_amove	*prev;
+	int 			x;
+	int				y;
+	int				etats;
+	//struct s_amove	*next;
+	//struct s_amove	*prev;
 }				t_amove;
 
 typedef struct	s_bmove
 {
-	int x;
-	int y;
-	int	etats;
-	s_bmove	*next;
-	s_bmove	*prev;
+	int 			x;
+	int 			y;
+	int				etats;
+	struct s_bmove	*next;
+	struct s_bmove	*prev;
 }				t_bmove;
+
+int		ft_atoi(const char *nptr);
+void	ft_display(struct s_amove **jeu, int widht, int height);
+t_amove	**ft_init_struct(int widht, int height);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
